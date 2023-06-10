8d30cb9b018ae4e7462c1c5ffb7ee2e9b21af610
Abdullah Ansari <aa9272137@gmail.com>
2023-06-10 21:54:39 +0400

    chore: added react import

--- src/Stories.tsx
+++ src/Stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 import {
     StyleSheet,
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Stories from "./Stories";
 
 export default Stories
