82797a09b563cf7f8617f8e81e397ef986f245a1
Abdullah Ansari <aa9272137@gmail.com>
2023-06-12 14:31:06 +0400

    docs: added multiline comments inside code

--- src/Stories.tsx
+++ src/Stories.tsx
@@ -23,19 +23,69 @@ type StoryType = Story & {
     finish: number
 };
 type PropTypes = {
+    /**
+     * An array of story objects
+     * @see https://www.npmjs.com/package/rn-story#story-object
+     */
     stories: Story[],
+    /**
+     * Set the current story index.
+     */
     currentIndex?: number,
+    /**
+     * Callback when the user taps/press to go back to the previous story.
+     */
     onPrevious?: Function,
+    /**
+     * Callback when the user taps/press to go back to the previous story but you are on the first story,
+     * i.e there are no more stories to go back (suitable for closing story view or update index to show previous profile story)
+     */
     onPreviousFirstStory?: Function,
+    /**
+     * Callback when the user taps/press to proceed to the next story
+     */
     onNext?: Function,
+    /**
+     * Callback when the user taps/press to proceed to next story but you are on the last story,
+     *  i.e there are no more stories to go forward (suitable for closing story view or update index to show next story)
+     */
     onAllStoriesEnd?: Function,
+    /**
+     * Control the volume of video.
+     */
     videoVolume?: number,
+    /**
+     * Switch to mute video.
+     * @default 1.0
+     */
     isMuted?: boolean,
+    /**
+     * Switch to changed the shape from rectangular animation bar to rounded.
+     * @default false
+     */
     isAnimationBarRounded?: boolean,
+    /**
+     * Modify the height of animation bar @default 2
+     */
     animationBarHeight?: number,
+    /**
+     * Modify the color of animation @default "#fff"
+     */
     animationBarColor?: string,
+    /**
+     * Change the text of **See More** button, *required `seeMoreUrl` to be set is Story Object.
+     * @default "View Details"
+     */
     seeMoreText?: string,
+    /**
+     * Override the styles of **See More** button container, *required `seeMoreUrl` to be set is Story Object.
+     * @default {}
+     */
     seeMoreStyles?: ViewStyle,
+    /**
+     * Override the styles of **See More** button text, *required `seeMoreUrl` to be set is Story Object.
+     * @default {}
+     */
     seeMoreTextStyles?: TextStyle,
 }
 export default function Stories({
@@ -302,7 +352,7 @@ export default function Stories({
 
 const styles = StyleSheet.create({
     container: {
-        
+
     },
     containerModal: {
         height: height,
